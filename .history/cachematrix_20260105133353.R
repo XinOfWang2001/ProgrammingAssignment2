941554080e258245946b936d16546ae48b42fe3a
Xin Wang <xx.wang@student.avans.nl>
2026-01-05 13:43:05 +0100

    Implemented the makeCacheMatrix and cacheSolve functions

--- .history/cachematrix_20260105133353.R
+++ .history/cachematrix_20260105133353.R
@@ -2,14 +2,7 @@
 ## functions do
 
 ## Write a short comment describing this function
-# This function contains a list of functions to:
-# set the value of the matrix
-# get the matrix value represented by x
-# set the value of the inverse matrix
-# get the value of the inverse matrix
-# The inv is set to NULL, because no matrix inversion has been calculated yet.
-# When calling the function, a function will be returned with all of these functions inside.
-# This will be used by the cacheSolve function to get the matrix inversion values from cache.
+
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
         set <- function(y) {
@@ -27,11 +20,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
-# Answer: The cacheSolve function calculates the inverse of the matrix 'x', after being created by the 'makeCacheMatrix'.
-# It will try to retrieve the inverse from memory (cache) to save compution time, in case the calculation was done before.
-# If that was the case, the cached value will be returned. Otherwise, it will call the get function to get x, setup earlier at makeCacheMatrix, 
-# call the solve function to perform matrix inversion and assigns it to the cache with the setinverse function.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inverse <- x$getinverse()
